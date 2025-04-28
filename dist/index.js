d179e96965c4762b44968c7d9b6e2f45c7cfa246
GuillemotClement <guillemot.clement@protonmail.com>
2025-04-28 23:51:01 +0200

    add middleware for login failed request

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,12 @@
 import express from "express";
+import { handlerReadiness } from "./api/readiness.js"; // importation du handler
+import middlewareLogResponses from "./middlewares/middlewareLogResponses.js";
 const app = express();
 const PORT = 8080; // declaration du port ecouter par le serveur
-app.use(express.static(".")); //definition du folder qui contient les fichiers statique
+app.use(middlewareLogResponses);
+app.use("/app", express.static("./src/app")); //definition du folder qui contient les fichiers statique
+app.get("/healthz", handlerReadiness); // ajout d'un handler pour traiter les requetes sur l'url
+// lancement du serveur
 app.listen(PORT, () => {
     console.log(`Server is listen as http://localhost:${PORT}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 import express from "express";
 
+import { handlerReadiness } from "./api/readiness.js"; // importation du handler
+import { middlewareLogResponse } from "./api/middleware.js"; // importation du middleware
+
 const app = express();
 const PORT = 8080; // declaration du port ecouter par le serveur
 
-app.use(express.static(".")); //definition du folder qui contient les fichiers statique
+app.use(middlewareLogResponse); // on ajoute le middleware sur toutes les requetes entrantes.
+
+app.use("/app", express.static("./src/app")); //definition du folder qui contient les fichiers statique
+
+app.get("/healthz", handlerReadiness); // ajout d'un handler pour traiter les requetes sur l'url
 
 // lancement du serveur
 app.listen(PORT, () => {
