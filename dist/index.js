fff7c1a757b09150e553b18ad5873b9acd42c562
GuillemotClement <guillemot.clement@protonmail.com>
2025-04-29 21:02:00 +0200

    Add middleware for clean word

--- dist/index.js
+++ dist/index.js
@@ -2,15 +2,17 @@ import express from "express";
 import { handlerReadiness } from "./api/readiness.js"; // importation du handler
 import { middlewareLogResponse, middlewareMetricsInc } from "./api/middleware.js"; // importation du middleware
 import { handlerCountRequests, handlerResetMetrics } from "./api/metrics.js";
-import { handleValidateChirp } from "./api/chirp.js";
+import { handlerChirpsValidate } from "./api/chirp.js";
 const app = express();
 const PORT = 8080; // declaration du port ecouter par le serveur
+// traitement des json
+app.use(express.json());
 app.use(middlewareLogResponse); // on ajoute le middleware sur toutes les requetes entrantes.
 app.use("/app", middlewareMetricsInc, express.static("./src/app")); //definition du folder qui contient les fichiers statique
 app.get("/api/healthz", handlerReadiness); // ajout d'un handler pour traiter les requetes sur l'url
 app.get("/admin/metrics", handlerCountRequests);
 app.post("/admin/reset", handlerResetMetrics);
-app.post("/api/validate_chirp", handleValidateChirp);
+app.post("/api/validate_chirp", handlerChirpsValidate);
 // lancement du serveur
 app.listen(PORT, () => {
     console.log(`Server is listen as http://localhost:${PORT}`);
--- src/api/chirp.ts
+++ src/api/chirp.ts
@@ -16,7 +16,25 @@ export async function handlerChirpsValidate(req: Request, res: Response) {
     return;
   }
 
+  const badWords = ["kerfuffle", "sharbert", "fornax"];
+
+  // on transforme la string en tableau en indiquant un espace pour le separateur
+  const words = params.body.split(" "); // recupere les elements de la requete
+
+  // on parcourt la liste de mods dans le body
+  const cleanWords = words.map((word) => {
+    // si le mot est dans la liste des bad words alors on remplace, sinon on retourne le mot
+    if (badWords.includes(word.toLowerCase())) {
+      return "****";
+    }
+    return word;
+  });
+
+  // on transforme le tableau en string en ajoutant un espace comme separateur
+  const stringWithGoodWords = cleanWords.join(" ");
+
+  // on retourne la string en reponse
   respondWithJSON(res, 200, {
-    valid: true,
+    cleanedBody: stringWithGoodWords,
   });
 }
