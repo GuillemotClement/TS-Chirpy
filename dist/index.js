06f186d13bc87879367b0bde5abbb3431c4d3be1
GuillemotClement <guillemot.clement@protonmail.com>
2025-04-29 20:12:53 +0200

    Add middleware json

--- dist/index.js
+++ dist/index.js
@@ -2,13 +2,15 @@ import express from "express";
 import { handlerReadiness } from "./api/readiness.js"; // importation du handler
 import { middlewareLogResponse, middlewareMetricsInc } from "./api/middleware.js"; // importation du middleware
 import { handlerCountRequests, handlerResetMetrics } from "./api/metrics.js";
+import { handleValidateChirp } from "./api/chirp.js";
 const app = express();
 const PORT = 8080; // declaration du port ecouter par le serveur
 app.use(middlewareLogResponse); // on ajoute le middleware sur toutes les requetes entrantes.
 app.use("/app", middlewareMetricsInc, express.static("./src/app")); //definition du folder qui contient les fichiers statique
 app.get("/api/healthz", handlerReadiness); // ajout d'un handler pour traiter les requetes sur l'url
 app.get("/admin/metrics", handlerCountRequests);
-app.get("/admin/reset", handlerResetMetrics);
+app.post("/admin/reset", handlerResetMetrics);
+app.post("/api/validate_chirp", handleValidateChirp);
 // lancement du serveur
 app.listen(PORT, () => {
     console.log(`Server is listen as http://localhost:${PORT}`);
--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,13 @@ import { handlerReadiness } from "./api/readiness.js"; // importation du handler
 import { middlewareLogResponse, middlewareMetricsInc } from "./api/middleware.js"; // importation du middleware
 import { handlerCountRequests, handlerResetMetrics } from "./api/metrics.js";
 import { config } from "./config.js";
+import { handlerChirpsValidate } from "./api/chirp.js";
 
 const app = express();
 const PORT = 8080; // declaration du port ecouter par le serveur
+
+// traitement des json
+app.use(express.json());
 
 app.use(middlewareLogResponse); // on ajoute le middleware sur toutes les requetes entrantes.
 
@@ -15,7 +19,9 @@ app.use("/app", middlewareMetricsInc, express.static("./src/app")); //definition
 app.get("/api/healthz", handlerReadiness); // ajout d'un handler pour traiter les requetes sur l'url
 
 app.get("/admin/metrics", handlerCountRequests);
-app.get("/admin/reset", handlerResetMetrics);
+app.post("/admin/reset", handlerResetMetrics);
+
+app.post("/api/validate_chirp", handlerChirpsValidate);
 
 // lancement du serveur
 app.listen(PORT, () => {
