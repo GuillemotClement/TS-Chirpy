7b556bb05f4e393d687c7bbbafd9957c494349bb
GuillemotClement <guillemot.clement@protonmail.com>
2025-04-29 23:27:42 +0200

    add connection to DB and createUser

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,20 @@
 type APIConfig = {
   fileserverHits: number;
+  dbURL: string;
 };
+
+process.loadEnvFile(); //charge le .env
+
+// functon qui permet de gerer si une valeur est absente
+function envOrThrow(key: string) {
+  const value = process.env[key];
+  if (!value) {
+    throw new Error(`Environment variable ${key} is not set`);
+  }
+  return value;
+}
 
 export const config = {
   fileserverHits: 0,
+  dbURL: envOrThrow("DB_URL"),
 };
